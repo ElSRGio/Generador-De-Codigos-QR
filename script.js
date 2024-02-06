712845a476317d5c5f3e3d4869f31ee7f8c7da56
Giovanni <158414388+ElSRGio@users.noreply.github.com>
2024-02-05 21:15:14 -0600

    Generador de Qr
    Genera QR y permite descargarlo

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Crea una instancia de QRCode
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+        width: 128,
+        height: 128
+    });
+
+    // Función para generar el código QR
+    window.generarQR = function () {
+        var inputText = document.getElementById("inputText").value;
+        qrcode.makeCode(inputText);
+    };
+
+    // Función para descargar el código QR
+    window.descargarQR = function () {
+        // Obtén el contenido del código QR directamente desde el canvas
+        var canvas = document.querySelector("#qrcode canvas");
+        var imageURL = canvas.toDataURL("image/png");
+        
+        // Crea un enlace temporal y simula el clic para descargar
+        var link = document.createElement("a");
+        link.href = imageURL;
+        link.download = "qrcode.png";
+        link.click();
+    };
+});
